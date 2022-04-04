97fa29a09e2138b328be5f1f6e77a97f493b49f3
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-04-04 13:02:11 +0700

    Add more minitalk

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 all: client server
 
 client:
-	gcc client.c ft_utils.c ft_utils_2.c -o client
+	gcc -g client.c ft_utils.c ft_utils_2.c -o client
 server:
-	gcc server.c ft_utils.c ft_utils_2.c -o server
+	gcc -g server.c ft_utils.c ft_utils_2.c -o server
 
 clean:
 
 fclean: clean
 	rm -rf client
 	rm -rf server
+
+re: fclean all
 
 norminette:
 	norminette server.c
--- server.c
+++ server.c
@@ -11,13 +11,57 @@
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <limits.h>
+#include <stdio.h>
+char *chartobin ( unsigned char c )
+{
+    static char bin[CHAR_BIT + 1] = {0};
+    int i;
+    for( i = CHAR_BIT - 1; i >= 0; i-- )
+    {
+        bin[i] = (c % 2) + '0';
+        c /= 2;
+    }
+   	return bin;
+}
+
+// void test_print_binary(void) {
+// 	char a = 42;
+// 	int i;
+// 	for (i = 0; i < 8; i++) {
+// 	//	ft_putnbr_fd(((a << i) & 0x80), 1);
+// 	//	ft_putstr_fd("x", 1);
+// 		ft_putnbr_fd(!!((a << i) & 0x80), 1);
+// 	}
+// 	ft_putstr_fd("\n", 1);
+// }
+
+static void	server_action(int sig, siginfo_t *info, void *context)
+{
+	// kill -s 10 <pid>
+	// kill -s 12 <pid>
+	printf("Bin 42 is %s\n", chartobin(42));
+	ft_putstr_fd("Hey Recieved : \n", 1);
+}
 
 int	main(void)
 {
-	int pid;
+	int					pid;
+	struct sigaction	s_sigaction;
 
+	// test_print_binary();
+
+	ft_memset(&s_sigaction, 0, sizeof(s_sigaction));
 	pid = getpid();
-	ft_putstr_fd("server, PID is : \n", 1);
+	ft_putstr_fd("Server, PID is : ", 1);
 	ft_putnbr_fd(pid, 1);
+	ft_putstr_fd("\n", 1);
+	s_sigaction.sa_sigaction = server_action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+
+	while (1)
+		pause();
 	return (0);
 }
