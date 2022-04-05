3482ad8e20ad8243bcd78d31efa490dba1fe524c
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-04-05 11:45:35 +0700

    Fix more better client

--- client.c
+++ client.c
@@ -11,94 +11,10 @@
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
-
-// static int server_pid = 0;
-// static char *current_str;
-
-// static void	kill_with_check(int pid, int sig)
-// {
-// 	int	ret;
-
-// 	ret = 0;
-// 	usleep(1000);
-// 	if (pid == -1 || pid == 0)
-// 	{
-// 		ft_putendl_fd("Not Support Process ID 0 or -1.", 1);
-// 		exit(0);
-// 	}
-// 	ret = kill(pid, sig);
-// 	if (ret == -1)
-// 	{
-// 		ft_putendl_fd("Invalid Process ID.", 1);
-// 		exit(0);
-// 	}
-// }
-
-// static void	client_action(int sig, siginfo_t *info, void *context)
-// {
-// 	static int	shifted_bit = 1;
-// 	static int	shifted_byte = 0;
-// 	char	c;
-
-// 	c = *(current_str + shifted_byte);
-// 	// write(1, &c, 1);
-
-	
-
-// 	if (sig == SIGUSR1 || sig == SIGUSR2)
-// 	{
-// 		if (shifted_bit >= 8)
-// 		{
-// 			shifted_bit = 0;
-// 			shifted_byte++;
-// 			ft_putstr_fd("x", 1);
-// 			if (*(current_str + shifted_byte) == '\0')
-// 			{
-// 				ft_putstr_fd("Send Complete! Byte sent : ", 1);
-// 				ft_putnbr_fd(shifted_byte, 1);
-// 				ft_putstr_fd("\n", 1);
-// 				ft_putstr_fd("z", 1);
-// 				kill_with_check(server_pid, SIGUSR1);
-// 				exit(0);
-// 			}
-// 		}
-// 		if ((c << shifted_bit++) & 0x80)
-// 		{
-// 			write(1, &"1", 1);
-// 			kill(server_pid, SIGUSR1);
-// 		}
-// 		else
-// 		{
-// 			write(1, &"0", 1);
-// 			kill(server_pid, SIGUSR2);
-// 		}
-// 	}
-	
-// }
-
-
-
-// static void	first_kill(char *str)
-// {
-// 	char	c;
-
-// 	c = *str;
-// 	if (c & 0x80)
-// 	{
-// 		write(1, &"1", 1);
-// 		kill_with_check(server_pid, SIGUSR1);
-// 	}
-// 	else
-// 	{
-// 		write(1, &"0", 1);
-// 		kill_with_check(server_pid, SIGUSR2);
-// 	}
-// }
 
 int	send_char(char p_c, pid_t p_pid)
 {
-	static int		bit = -1;
+	static int		i = -1;
 	static char		c;
 	static pid_t	pid;
 
@@ -107,19 +23,19 @@ int	send_char(char p_c, pid_t p_pid)
 		c = p_c;
 		pid = p_pid;
 	}
-	if (bit < 0)
-		bit = __CHAR_BIT__ * sizeof(c) - 1;
+	if (i < 0)
+		i = 7;
 	if (kill(pid, 0) < 0)
 	{
-		ft_putendl_fd("ERROR : cant send sig to pid : %d\n", pid);
-		exit(EXIT_FAILURE);
+		ft_putendl_fd("Invalid Process ID.", 1);
+		exit(0);
 	}
-	if (c & (1 << bit))
+	if (c & (1 << i))
 		kill(pid, SIGUSR1);
 	else
 		kill(pid, SIGUSR2);
-	bit--;
-	return (bit);
+	i--;
+	return (i);
 }
 
 void	send_str(char *p_str, pid_t p_pid)
@@ -145,9 +61,7 @@ void	send_str(char *p_str, pid_t p_pid)
 			bit = send_char(0, pid);
 	}
 	else
-	{
 		bit = send_char(0, 0);
-	}
 }
 
 void	client_action(int sig, siginfo_t *info, void *context)
@@ -156,7 +70,7 @@ void	client_action(int sig, siginfo_t *info, void *context)
 	if (sig == SIGUSR1)
 		send_str("", 0);
 	else if (sig == SIGUSR2)
-		exit(EXIT_SUCCESS);
+		exit(0);
 }
 
 int	main(int argc, char **argv)
@@ -172,15 +86,12 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	s_sigaction.sa_sigaction = client_action;
-	s_sigaction.sa_flags = SA_SIGINFO | SA_RESTART | SA_NODEFER;
+	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
 	pid = ft_atoi(argv[1]);
-	// current_str = argv[2];
-	// first_kill(current_str);
 	send_str(argv[2], pid);
 	while (1)
 		pause();
 	return (0);
 }
-
